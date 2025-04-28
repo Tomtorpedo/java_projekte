2cdd63f64ac8d139605b02a5cab6b7b660fa488d
tomtorpedo <vervoort.tom@proton.me>
2025-04-28 13:38:31 +0200

    asdfghjkl

---
+++ hello-ide/src/HelloIntellij.java
@@ -0,0 +1,7 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class HelloIntellij {
+    public static void main(String[] args) {
+        System.out.println("Hello World\nHallo HS Bund!");
+    }
+    }
