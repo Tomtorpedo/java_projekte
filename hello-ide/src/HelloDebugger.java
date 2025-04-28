7dd88982cc2d5e3d712e9a36468e6ac3264a3d80
tomtorpedo <vervoort.tom@proton.me>
2025-04-28 13:33:03 +0200

    asdfghjkl

---
+++ hello-ide/src/HelloDebugger.java
@@ -0,0 +1,13 @@
+public class HelloDebugger {
+    public static void main(String[] args) {
+        int a=6;
+        str b;
+        b=System.in.read();
+
+        //calculate the product
+        int product =6*7;
+
+        //print result
+        System.err.println(a+"*"+b+"="+product);
+    }
+}
