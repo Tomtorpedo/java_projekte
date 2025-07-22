91c8ebab6f05ce065f9e7e92d72ff61333654d4b
tomtorpedo <vervoort.tom@proton.me>
2025-07-22 08:51:37 +0200

    dfghjklö

--- B-Aufgaben/src/Main.java
+++ B-Aufgaben/src/Main.java
@@ -2,9 +2,7 @@ public class Main {
     public static void main(String[] args) {
         Car car = new Car("VW Golf",42617);
         Motorcycle motorcycle = new Motorcycle("Simson Schwalbe",9876,true);
-        System.out.println(car.toString());
-        System.out.println(motorcycle.toString());
-//        car.start();
-//        motorcycle.start();
+        System.out.println(car);
+        System.out.println(motorcycle);
     }
 }
